c810f431b5bf51b6c3b4084daf2b6912eca57f28
Atalanta Dey <atalantadey2003@gmail.com>
2025-02-16 19:01:04 +0530

    updated Intro Screen

--- lib/features/presentation/screens/intro_screen.dart
+++ lib/features/presentation/screens/intro_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
-import 'package:projects/features/presentation/screens/home_screen.dart';
+import 'package:projects/features/auth/presentation/screens/login_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
@@ -15,40 +15,43 @@ class IntroScreen extends StatelessWidget {
             body:
                 "Effortlessly organize your tasks with AI-powered assistance and a clean, intuitive design.",
             image: Center(
-                child: Image.asset('assets/images/projects_logo.png',
-                    height: 300.0)),
+              child: Image.asset(
+                'assets/images/projects_logo.png',
+                height: 300.0,
+              ),
+            ),
           ),
           PageViewModel(
             title: "Organize Your Day in Seconds",
             body:
                 "Easily add, edit, and delete tasks using our AI chatbot or simple voice commands.",
             image: Center(
-                child: Image.asset('assets/images/projects_logo.png',
-                    height: 175.0)),
+              child:
+                  Image.asset('assets/images/login_tasks.jpg', height: 300.0),
+            ),
           ),
           PageViewModel(
             title: "Stay Focused, Stay Ahead",
             body:
                 "Prioritize tasks, set deadlines, and boost your productivity with ease.",
             image: Center(
-                child: Image.asset('assets/images/projects_logo.png',
-                    height: 175.0)),
+                child: Image.asset('assets/images/intro_3.jpg', height: 300.0)),
           ),
         ],
         onDone: () {
           Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (_) => HomeScreen()));
+              context, MaterialPageRoute(builder: (_) => LoginScreen()));
         },
         done: const Text(
           "Get Started",
           style: TextStyle(color: Colors.white),
         ),
         doneStyle: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.green),
+          backgroundColor: WidgetStateProperty.all(Colors.green),
         ),
         showNextButton: false,
         autoScrollDuration: 4000,
-        globalBackgroundColor: Colors.green[50],
+        globalBackgroundColor: Colors.green[30],
       ),
     );
   }
